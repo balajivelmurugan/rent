a9624764035e5b3298686d833a46b45a0a6f2bda
balajivelmurugan <balaji.velu2204@gmail.com>
2025-08-23 18:42:20 +0530

    fixed app context url

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -3,7 +3,7 @@ export function register() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/service-worker.js")
+        .register("/rent/service-worker.js")
         .then((registration) => {
           console.log("SW registered: ", registration);
         })
